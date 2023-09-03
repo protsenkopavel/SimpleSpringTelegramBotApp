c8b05330ad9fb7e434e585a094c20ff7d5e1a0a1
pavel <pavelprotsenko@outlook.com>
2023-09-03 14:40:38 +0300

    Log system

--- src/main/java/com/example/SimpleSpringTelegramBot/config/BotInitializer.java
+++ src/main/java/com/example/SimpleSpringTelegramBot/config/BotInitializer.java
@@ -1,6 +1,7 @@
 package com.example.SimpleSpringTelegramBot.config;
 
 import com.example.SimpleSpringTelegramBot.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -9,6 +10,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
 @Component
 public class BotInitializer {
 
@@ -18,7 +20,11 @@ public class BotInitializer {
     @EventListener({ContextRefreshedEvent.class})
     public void init() throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        telegramBotsApi.registerBot(bot);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
     }
 
 }
